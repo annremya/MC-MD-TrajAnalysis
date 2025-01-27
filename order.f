b0bd7b4c40298a7c13b823aef1911d5334f5dbdf
Remya Mathews <40625281+annremya@users.noreply.github.com>
2025-01-27 18:52:50 +0530

    Orientation Order Parameter

--- order.f
+++ order.f
@@ -23,20 +23,16 @@
 	nhis = 500
 	rcut = 2.0d0
 	ind = 0
-!	box = 26.7261 ! 0.7
-	box = 25.0d0 !0.8
-!	box = 23.5702 !0.9
-!	box = 22.3607 ! 1
 	Print *, 'Enter the number of frames: '
 	read *, frames
 
 	!--------reading vmd file-------------------------------	
 	do 15 k = 1,frames
-	   open(11, file = 'config8.xyz')
+	   open(11, file = 'runningprod.xyz')
 	   Read(11,*) Nt
 	   Read(11,*)
 !	   Read(11,1100) dum, dummy, dummy, dummy
-!	   Read(11,1100) dum, box2, dummy, dummy
+	   Read(11,1100) dum, box2, dummy, dummy
 !	   Read(11,1100) dum, dummy, dummy, dummy
 !	   Read(11,1100) dum, dummy, dummy, dummy
 !	   Read(11,1100) dum, dummy, dummy, dummy
@@ -44,20 +40,29 @@
 !	   Read(11,1100) dum, dummy, dummy, dummy 
 !	   Read(11,1100) dum, dummy, dummy, dummy
 	   Do i = 1, N
-	      Read(11,*) He1(i),rx(i), ry(i), rz(i)
-!	      do j = 1, Np
-!		   Read(11, 1100) He2(i) , ex(i,j), ey(i,j), ez(i,j)
-!	      enddo
+	      Read(11,1100) He1(i),rx(i), ry(i), rz(i)
+	      do j = 1, Np
+		   Read(11, 1100) He2(i) , ex(i,j), ey(i,j), ez(i,j)
+	      enddo
 	    Enddo
 !	    print *, 'box2', box2
 
-!	    Do i = 1, N
-!	       Do j = 1, Np
-!		    fx(i,j) = 20.0*(ex(i,j)-rx(i))	!unit patch vector for 50  patch coverages
-!		    fy(i,j) = 20.0*(ey(i,j)-ry(i))	!since He2 is written as rx+(ex/20.0)
-!	       Enddo
-!	    Enddo
-
+	    Do i = 1, N
+	       Do j = 1, Np
+		    fx(i,j) = 20.0*(ex(i,j)-rx(i))	!unit patch vector for 50  patch coverages
+		    fy(i,j) = 20.0*(ey(i,j)-ry(i))	!since He2 is written as rx+(ex/20.0)
+	       Enddo
+	    Enddo
+	 !--------initialise P(n1.n2)------------------------------
+	   If (k.eq.1) then
+	      box = box2*2.0
+	      ngr = 0
+	      deln = 2.0/(2.0*dble(nhis))
+	      print *,box, box2, deln
+	      do i = 1, 2*nhis
+	         p(i) = 0
+		enddo
+	   Endif
 	!-----------sample P(n1.n2)---------------------------------
 	   ngr = ngr+1
 	   do i = 1, N-1
@@ -73,28 +78,43 @@
 	     	      If (rij .lt. 1.0d0) then
 	    	         print *, rij, 'Overlapping particle'
 		         go to 16
-            	      else
-!			print *, rij, 'rij is greater than 1'
-!		         do a = 1, Np
-!		          ndot = (fx(i,a)*fx(j,a))+(fy(i,a)*fy(j,a))
-!			  theta = acos(real(ndot))
-!			  theta = (180.0/3.14)*theta
+            	      elseif (rij .lt. 1.05d0) then
+		         do a = 1, Np
+		          ndot = (fx(i,a)*fx(j,a))+(fy(i,a)*fy(j,a))
+			  theta = acos(real(ndot))
+			  theta = (180.0/3.14)*theta
 !			  print *, 'ndot',ndot
 !			  print *, 'theta',theta
-!		         enddo
-!		         ind = int((real(ndot)+1.0)/deln)+1	
+		         enddo
+		         ind = int((real(ndot)+1.0)/deln)+1	
 !			 print *, 'ind', ind
-!		         p(ind) = p(ind)+2
+		         p(ind) = p(ind)+2
 		      Endif
 		   Endif
 16		continue
 	   enddo
 
+         If(mod (k, 10000) .eq. 0) then
+	   print *, k
+	 Endif
+
 15 	continue
+	   print*,'ngr is', ngr
+	   do i = 1, 2*nhis
+!	      print *,  p(i)
+	   enddo
+
+	!-----------calculate P(n1.n2)-----------------------------
+	do i =1, (2*nhis)
+	   dist = (deln*(dble(i)+0.5))-1.0
+!	   print *, i, dist
+	   p(i) = p(i)/(dble(ngr*N)) 
+	   open(17,file='ndot.dat')
+	   write(17, *) dist, p(i)
+	enddo
 
 
-
-!1100	Format(2X, 1A, 3(F14.7, 2X))
+1100	Format(2X, 1A, 3(F14.7, 2X))
 
 
 	Stop
